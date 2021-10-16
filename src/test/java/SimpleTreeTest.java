993a7afda0bae3d5fa9f7e6ee62a6a6e3c3e7e59
Dmitry Malofeev <dim2k2006@yandex.ru>
2021-10-16 15:56:33 +0200

    Initial

---
+++ src/test/java/SimpleTreeTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTreeTest {
+    @Test
+    @DisplayName("Should work")
+    void shouldAddValueToFilter() {
+        Assertions.assertTrue(true);
+    }
+}
