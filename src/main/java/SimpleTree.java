a65e29245ed77e61b733d567be9a06b127a4d283
Dmitry Malofeev <dim2k2006@yandex.ru>
2021-10-16 22:16:24 +0200

    Update "SimpleTree" class

--- src/main/java/SimpleTree.java
+++ src/main/java/SimpleTree.java
@@ -1,5 +1,17 @@
 import java.util.*;
 
+public class SimpleTreeNode<T> {
+  public T NodeValue; // значение в узле
+  public SimpleTreeNode<T> Parent; // родитель или null для корня
+  public List<SimpleTreeNode<T>> Children; // список дочерних узлов или null
+
+  public SimpleTreeNode(T val, SimpleTreeNode<T> parent) {
+    NodeValue = val;
+    Parent = parent;
+    Children = null;
+  }
+}
+
 class SimpleTree<T> {
   public SimpleTreeNode<T> Root; // корень, может быть null
 
